99a4c0c86592b67d55b2dcd610c0db46d78eae5e
Sean Wallace <sawallace7@gmail.com>
2022-08-18 16:28:52 -0700

    changes to not notFound

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,22 +1,17 @@
-import React, { Component } from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import About from './pages/About'
-import CreateListing from './pages/CreateListing'
-import Home from './pages/Home'
-import Index from './pages/Index'
-import LoggedInHome from './pages/LoggedInHome'
-import NotFound from './pages/NotFound'
-import Review from './pages/Review'
-import ReviewEdit from './pages/ReviewEdit'
-import Show from './pages/Show'
-import UpdateOrDeleteListing from './pages/UpdateOrDeleteListing'
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from 'react-router-dom'
-
+import React, { Component } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import About from "./pages/About";
+import CreateListing from "./pages/CreateListing";
+import Home from "./pages/Home";
+import Index from "./pages/Index";
+import LoggedInHome from "./pages/LoggedInHome";
+import NotFound from "./pages/NotFound";
+import Review from "./pages/Review";
+import ReviewEdit from "./pages/ReviewEdit";
+import Show from "./pages/Show";
+import UpdateOrDeleteListing from "./pages/UpdateOrDeleteListing";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
@@ -25,38 +20,39 @@ class App extends Component {
       current_user,
       new_user_route,
       sign_in_route,
-      sign_out_route
-    } = this.props
-    
-    console.log("logged_in:", logged_in)
-    console.log("current_user:", current_user)
-    console.log("new_user_route:", new_user_route)
-    console.log("sign_in_route:", sign_in_route)
-    console.log("sign_out_route:", sign_out_route)
-    return(
+      sign_out_route,
+    } = this.props;
+
+    console.log("logged_in:", logged_in);
+    console.log("current_user:", current_user);
+    console.log("new_user_route:", new_user_route);
+    console.log("sign_in_route:", sign_in_route);
+    console.log("sign_out_route:", sign_out_route);
+    return (
       <>
         <Router>
           <Header />
-          <h1>
-            This is the Home page
-          </h1>
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/about" component={About} />
-              <Route path="/create_listing" component={CreateListing} />
-              <Route path="/index" component={Index} />
-              <Route path="/logged_in_home" component={LoggedInHome} />
-              <Route component={NotFound} />
-              <Route path="/review" component={Review} />
-              <Route path="/review_edit" component={ReviewEdit} />
-              <Route path="/show" component={Show} />
-              <Route path="/update_or_delete_listing" component={UpdateOrDeleteListing} />
-            </Switch>
+          <h1>This is the Home page</h1>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/create_listing" component={CreateListing} />
+            <Route path="/index" component={Index} />
+            <Route path="/logged_in_home" component={LoggedInHome} />
+            <Route component={NotFound} />
+            <Route path="/review" component={Review} />
+            <Route path="/review_edit" component={ReviewEdit} />
+            <Route path="/show" component={Show} />
+            <Route
+              path="/update_or_delete_listing"
+              component={UpdateOrDeleteListing}
+            />
+          </Switch>
           <Footer />
         </Router>
       </>
-    )
+    );
   }
 }
 
-export default App
+export default App;
