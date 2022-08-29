bb5f8e2b1c6cbf9a59fd9c61fc6d02a69b9a5340
ninerboy81 <luis.ramos07@yahoo.com>
2022-08-29 10:00:29 -0700

    Fixed Delete and indented code

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,142 +1,135 @@
-import React, { Component } from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import About from './pages/About'
-import CreateListing from './pages/CreateListing'
-import Home from './pages/Home'
-import Index from './pages/Index'
-import LoggedInHome from './pages/LoggedInHome'
-import NotFound from './pages/NotFound'
-import CreateReview from './pages/CreateReview'
-import UpdateOrDeleteReview from './pages/UpdateOrDeleteReview'
-import Show from './pages/Show'
-import UpdateOrDeleteListing from './pages/UpdateOrDeleteListing'
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-} from 'react-router-dom'
-import reviews from './MockReviews'
+import React, { Component } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import About from "./pages/About";
+import CreateListing from "./pages/CreateListing";
+import Home from "./pages/Home";
+import Index from "./pages/Index";
+import LoggedInHome from "./pages/LoggedInHome";
+import NotFound from "./pages/NotFound";
+import CreateReview from "./pages/CreateReview";
+import UpdateOrDeleteReview from "./pages/UpdateOrDeleteReview";
+import Show from "./pages/Show";
+import UpdateOrDeleteListing from "./pages/UpdateOrDeleteListing";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import reviews from "./MockReviews";
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       listings: [],
       reviews: reviews,
-      search: false
-    }
+      search: false,
+    };
   }
-  
+
   readListing = () => {
     fetch("/listings")
-    .then(response => response.json())
-    .then(listingArr => this.setState({ listings: listingArr }))
-    .catch(errors => console.log("Listing read errors:", errors))
-  }
+      .then((response) => response.json())
+      .then((listingArr) => this.setState({ listings: listingArr }))
+      .catch((errors) => console.log("Listing read errors:", errors));
+  };
 
-    createNewListing= (theNewListing) =>{
-    fetch("/listings",{
-    body: JSON.stringify(theNewListing),
-    headers: {
-      "Content-Type": "application/json"
-    },
-    method:"POST"
+  createNewListing = (theNewListing) => {
+    fetch("/listings", {
+      body: JSON.stringify(theNewListing),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
     })
-    .then(response => response.json())
-    .then(() => this.readListing())
-    .catch(errors => console.log("New listing errors: ", errors))
-    }
-  
-    updateListing = (listing, id) => {
-    fetch(`/listings/${id}`,{
-    body: JSON.stringify(listing),
-    headers: {
-      "Content-Type": "application/json"
-    },
-    method:"PATCH"
-    })
-    .then(response => response.json())
-    .then(()=> this.readListing())
-    .catch(errors => console.log("Update listing errors: ", errors))
-  }
+      .then((response) => response.json())
+      .then(() => this.readListing())
+      .catch((errors) => console.log("New listing errors: ", errors));
+  };
 
-    deleteListing = (id) => {
+  updateListing = (listing, id) => {
+    fetch(`/listings/${id}`, {
+      body: JSON.stringify(listing),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "PATCH",
+    })
+      .then((response) => response.json())
+      .then(() => this.readListing())
+      .catch((errors) => console.log("Update listing errors: ", errors));
+  };
+
+  deleteListing = (id) => {
     fetch(`/listings/${id}`, {
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      method: "DELETE"
+      method: "DELETE",
     })
-    .then(response => response.json())
-    .then(() => this.readListing())
-    .catch(errors => console.log("Delete listing errors:", errors))
-  }
-  
+      .then((response) => response.json())
+      .then(() => this.readListing())
+      .catch((errors) => console.log("Delete listing errors:", errors));
+  };
 
   readReview = () => {
     fetch("/reviews")
-    .then(response => response.json())
-    .then(reviewArr => this.setState({ review: reviewArr }))
-    .catch(errors => console.log("Review read errors:", errors))
-  }
+      .then((response) => response.json())
+      .then((reviewArr) => this.setState({ review: reviewArr }))
+      .catch((errors) => console.log("Review read errors:", errors));
+  };
 
-    createNewReview= (theNewReview, listingID) =>{
-    fetch(`/reviews`,{
-    body: JSON.stringify(theNewReview),
-    headers: {
-      "Content-Type": "application/json"
-    },
-    method:"POST"
+  createNewReview = (theNewReview, listingID) => {
+    fetch(`/reviews`, {
+      body: JSON.stringify(theNewReview),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
     })
-    .then(response => response.json())
-    .then(() => this.readReview())
-    .catch(errors => console.log("New review errors: ", errors))
-    }
-  
-    updateReview = (review, id) => {
-    fetch(`/reviews/${id}`,{
-    body: JSON.stringify(review),
-    headers: {
-      "Content-Type": "application/json"
-    },
-    method:"PATCH"
-    })
-    .then(response => response.json())
-    .then(()=> this.readReview())
-    .catch(errors => console.log("Update review errors: ", errors))
-    }
+      .then((response) => response.json())
+      .then(() => this.readReview())
+      .catch((errors) => console.log("New review errors: ", errors));
+  };
 
-    deleteReview = (id) => {
+  updateReview = (review, id) => {
+    fetch(`/reviews/${id}`, {
+      body: JSON.stringify(review),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "PATCH",
+    })
+      .then((response) => response.json())
+      .then(() => this.readReview())
+      .catch((errors) => console.log("Update review errors: ", errors));
+  };
+
+  deleteReview = (id) => {
     fetch(`/reviews/${id}`, {
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      method: "DELETE"
+      method: "DELETE",
     })
-    .then(response => response.json())
-    .then(() => this.readReview())
-    .catch(errors => console.log("Delete review errors:", errors))
-    }
+      .then((response) => response.json())
+      .then(() => this.readReview())
+      .catch((errors) => console.log("Delete review errors:", errors));
+  };
 
-
-
-  handleSubmit = (event, form,) => {
-    event.preventDefault()
-    let values = { values: this.getKeyByValue(form, true) }
-    let query = new URLSearchParams(values).toString()
+  handleSubmit = (event, form) => {
+    event.preventDefault();
+    let values = { values: this.getKeyByValue(form, true) };
+    let query = new URLSearchParams(values).toString();
     fetch(`/search?${query}`)
-      .then(response => response.json())
-      .then((data) => this.setState({ listings: data, search: true }))
+      .then((response) => response.json())
+      .then((data) => this.setState({ listings: data, search: true }));
   };
 
   getKeyByValue = (object, value) => {
-      return Object.keys(object).filter(key => object[key] === value);
+    return Object.keys(object).filter((key) => object[key] === value);
   };
 
   resetSearch = () => {
-    this.setState({search: false})
-  }
+    this.setState({ search: false });
+  };
 
   render() {
     const {
@@ -147,43 +140,80 @@ class App extends Component {
       sign_out_route,
     } = this.props;
 
-    return(
+    return (
       <>
         <Router>
           <Header {...this.props} />
-            {this.state.listings.length > 0 && this.state.search && (
-              <Index listings={this.state.listings} resetSearch={this.resetSearch}/>
-            )}
-            <Switch>
-            <Route exact path="/" render={() => <Home handleSubmit={this.handleSubmit}/>}/>
+          {this.state.listings.length > 0 && this.state.search && (
+            <Index
+              listings={this.state.listings}
+              resetSearch={this.resetSearch}
+            />
+          )}
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={() => <Home handleSubmit={this.handleSubmit} />}
+            />
             <Route path="/about" component={About} />
-            <Route path="/create_listing"
-              render={() => <CreateListing createNewListing={this.createNewListing} current_user={current_user} />} />
-            <Route path="/logged_in_home" component={LoggedInHome} />
-            <Route path="/create_review"
-              render={() => <CreateReview createReview={this.createReview} current_user={this.props.current_user} />} />
-            <Route path="/update_or_delete_review/:id" render={(props) => {
-              let id = +props.match.params.id
-              let review = this.state.reviews.find(review => review.id === id)
-              return <UpdateOrDeleteReview {...props} review={review} />
-            }}/>
-            <Route path="/listing/:id" render={(props) => {
-                let id = +props.match.params.id
-                let listing = this.state.listings.find(listing => listing.id === id)
-                return( 
-                <Show 
-                {...props} 
-                listing={listing}
+            <Route
+              path="/create_listing"
+              render={() => (
+                <CreateListing
+                  createNewListing={this.createNewListing}
+                  current_user={current_user}
                 />
-                )
-              }} />
-            <Route path="/update_or_delete_listing/:id" render={(props) => {
-              let id = +props.match.params.id
-              let listing = this.state.reviews.find(listing => listing.id === id)
-              return <UpdateOrDeleteListing {...props} listing={listing} />
-            }}/>
+              )}
+            />
+            <Route path="/logged_in_home" component={LoggedInHome} />
+            <Route
+              path="/create_review"
+              render={() => (
+                <CreateReview
+                  createReview={this.createReview}
+                  current_user={this.props.current_user}
+                />
+              )}
+            />
+            <Route
+              path="/update_or_delete_review/:id"
+              render={(props) => {
+                let id = +props.match.params.id;
+                let review = this.state.reviews.find(
+                  (review) => review.id === id
+                );
+                return <UpdateOrDeleteReview {...props} review={review} />;
+              }}
+            />
+            <Route
+              path="/listing/:id"
+              render={(props) => {
+                let id = +props.match.params.id;
+                let listing = this.state.listings.find(
+                  (listing) => listing.id === id
+                );
+                return (
+                  <Show
+                    {...props}
+                    listing={listing}
+                    deleteListing={this.deleteListing}
+                  />
+                );
+              }}
+            />
+            <Route
+              path="/update_or_delete_listing/:id"
+              render={(props) => {
+                let id = +props.match.params.id;
+                let listing = this.state.reviews.find(
+                  (listing) => listing.id === id
+                );
+                return <UpdateOrDeleteListing {...props} listing={listing} />;
+              }}
+            />
             <Route path="/not_found" component={NotFound} />
-            </Switch>
+          </Switch>
           <Footer />
         </Router>
       </>
